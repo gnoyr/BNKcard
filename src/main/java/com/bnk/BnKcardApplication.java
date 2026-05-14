db602366d698f96c33db3da18a58cc8c3680ea2b
ksr <ryong0406@naver.com>
2026-05-14 15:50:10 +0900

    make project

---
+++ src/main/java/com/bnk/BnKcardApplication.java
@@ -0,0 +1,13 @@
+package com.bnk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BnKcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BnKcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnk/BnKcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnKcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
