b2f65eadf4a2d306f62b0bbce25ccbe5706b6f88
ksr <ryong0406@naver.com>
2026-05-18 17:38:22 +0900

    [feat]제미나이api 환경 설정

--- src/main/java/com/bnk/domain/spending/service/SpendingService.java
+++ src/main/java/com/bnk/domain/spending/service/SpendingService.java
@@ -2,6 +2,7 @@ package com.bnk.domain.spending.service;
 
 import java.util.List;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -15,10 +16,22 @@ import jakarta.validation.Valid;
 @Service
 @Validated
 public class SpendingService {
+	private final ChatClient chatClient;
 
+	public SpendingService(ChatClient.Builder builder) {
+        this.chatClient = builder
+            .defaultSystem("너는 한국어로 답변하는 친절한 어시스턴트다.")
+            .build();
+    }
+	
 	public AiChatResponse chat(@Valid AiChatRequest request, Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		String result = chatClient.prompt()
+				        .user(request.getUserInput())
+				        .call()
+				        .content();
+		AiChatResponse done = AiChatResponse.builder().chatId(null).sessionId(null).response(result).build();
+		
+        return done;
 	}
 
 	public List<SpendingChartResponse> getMySpendingPatterns(Long userId) {
