0b0ff1c2131c98b163d930a1e10cb0ad0d5f6ec7
psh <thgus6674@naver.com>
2026-05-26 18:41:52 +0900

    front

--- src/main/java/com/bnk/domain/card/dto/request/CardUpdateRequest.java
+++ src/main/java/com/bnk/domain/card/dto/request/CardUpdateRequest.java
@@ -48,4 +48,9 @@ public class CardUpdateRequest {
 
     @Pattern(regexp = "Y|N")
     private String visibleYn;
+    
+    @Pattern(regexp = "Y|N")
+    private String deletedYn;
+    
+    private String cardStatus;
 }
