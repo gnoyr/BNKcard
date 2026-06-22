bc62032383c8e8a737e2b84b05549a74f5e80f17
psh <thgus6674@naver.com>
2026-06-22 11:16:03 +0900

    card_application

--- src/main/java/com/bnk/domain/application/mapper/UserCardMapper.java
+++ src/main/java/com/bnk/domain/application/mapper/UserCardMapper.java
@@ -1,5 +1,6 @@
 package com.bnk.domain.application.mapper;
 
+import com.bnk.domain.application.dto.response.MyCardApplicationResponse;
 import com.bnk.domain.application.model.UserCard;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -15,4 +16,8 @@ public interface UserCardMapper {
 
     int updateCardStatus(@Param("userCardId") Long userCardId,
                          @Param("cardStatus") String cardStatus);
+    
+    // 신용/체크카드 통합 신청 현황 조회 (날짜순)
+    List<MyCardApplicationResponse> findMyAllApplications(@Param("userId") Long userId);
+    // 이건 마이페이지 쪽 서비스에서 구현해야함 !!
 }
